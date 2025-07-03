00686b1306a5e83f40fd671af6d33666be4124ed
reeezmg <64250221+reeezmg@users.noreply.github.com>
2025-07-03 09:59:58 +0530

    report fix

--- composables/firebase.ts
+++ composables/firebase.ts
@@ -1,6 +1,6 @@
+// ~/composables/firebase.ts
 import { initializeApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-import { getMessaging, getToken, onMessage } from 'firebase/messaging'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBIdT1rcRgxGuPbIE8o3iHellO306YXWvU",
@@ -9,11 +9,16 @@ const firebaseConfig = {
   storageBucket: "markit-e2b0e.appspot.com",
   messagingSenderId: "1024139443483",
   appId: "1:1024139443483:web:df6200650e2c956c54e7be",
-  vapidKey: 'BNxKrA3tmbBLhIMNTd8ghKsYf5nrujaprB0ulxf9_rAx8O8BZuLTEGQiMA_mPyq9yCNnn1g65m-3rqJlMu7d480'
 }
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig)
-const messaging = getMessaging(app)
 const auth = getAuth(app)
 
-export { app, auth,messaging }
+let messaging: any = null
+
+if (process.client) {
+  const { getMessaging } = await import('firebase/messaging')
+  messaging = getMessaging(app)
+}
+
+export { app, auth, messaging }
