2eeb5bc1661d6e0572ebe996f36e2ee310cc0593
reeezmg <64250221+reeezmg@users.noreply.github.com>
2025-08-20 21:01:17 +0530

    Add CORS middleware

--- auth/server/api/auth/session.get.ts
+++ auth/server/api/auth/session.get.ts
@@ -1,8 +1,5 @@
 export default eventHandler(async (event) => {
   // 🚫 Disable caching for this route
-  setHeader(event, 'Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-  setHeader(event, 'Pragma', 'no-cache');
-  setHeader(event, 'Expires', '0');
 
   const session = await useAuthSession(event);
   return session.data;
